e469761bbf14157a8a13684b673db3d911a529c0
Владислав Трофимченко <vladislavtrofimcenko@MacBook-Air-Vladislav-2.local>
2023-12-15 19:23:33 +0300

    fix

--- src/main/java/com/java/course/isdb/dto/request/AddAdminRequest.java
+++ src/main/java/com/java/course/isdb/dto/request/AddAdminRequest.java
@@ -1,4 +1,3 @@
 package com.java.course.isdb.dto.request;
 
-public record AddAdminRequest(String name, int age, String division) {
-}
+public record AddAdminRequest(String name, int age, String division) { }
